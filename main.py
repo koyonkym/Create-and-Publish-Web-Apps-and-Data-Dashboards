6f5fa1ead12db62d507e5b7a8f9522b0f1c35ddf
KOYO <35590527+koyonkym@users.noreply.github.com>
2022-05-29 05:38:23 +0000

    Add slider

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ st.write('''This is our first Web App.
          ''')
 st.write(df)
 st.line_chart(df)
+st.area_chart(df)
+
+myslider = st.slider('Celsius')
+st.write(myslider, 'in Fahreheit is', myslider * 9/5 + 32)
